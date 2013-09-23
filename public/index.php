efb74cc99e17b112219be80e968ae26f3d247d40
Rich Delauder <rich@Dev-3.local>
2013-09-22 20:43:15 -0700

    added index view

--- public/index.php
+++ public/index.php
@@ -3,12 +3,23 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application(); 
 
+$env = require( __DIR__ . '/../.env.php');
+foreach ($env as $k => $v){
+	putenv($k.'='.$v);
+}
+
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-	'twig.path' => __DIR__.'/views',
+	'twig.path' => __DIR__.'/../views',
+	'twig.options' => ['debug' => true]
 ));
 
 $app->get('/hello/{name}', function($name) use($app) { 
-	return 'Hello '.$app->escape($name); 
+	return 'Hello'.$app->escape($name);
 }); 
+
+$app->get('/', function() use($app) { 
+	return $app['twig']->render('index.html.twig',['pusher_key' => getenv('PUSHER_KEY')]);
+}); 
+
 
 return $app;
